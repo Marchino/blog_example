97fea3660213d8550bc680a5d593575af391a939
Marchino <marco.crepaldi@doctolib.com>
2023-05-31 16:58:12 +0200

    Add authors and link them to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
     def index
-        @articles = Article.published
+        @articles = Article.includes(:author).published
         respond_to do |format|
             format.html
             format.json do 
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ApplicationRecord
     scope :published, -> { where(published: true) }
+    belongs_to :author
 end
